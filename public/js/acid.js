b26cd9888a2c89d3aac45f39d083fff884aeb508
RedWings <korylinkin@gmail.com>
2017-06-04 20:07:40 -0300

    un poco de orden en codigo

---
+++ public/js/acid.js
@@ -0,0 +1,18 @@
+$(function(){
+	score=-1;
+		var clicker = document.getElementById('clicker');
+		clicker.addEventListener('click', function(){
+			var msg = {
+				0: "stop",
+				1: "clicking",
+				2: "on",
+				3: "me",
+				4: "you",
+				5: "motherfucker",
+			};	
+			score++;
+			var i = score % 6;
+			document.getElementById('clicker').innerHTML = msg[i];
+			document.getElementById('t1').value = "score: " + score;
+		});
+})
--- vistas/clicker.php
+++ vistas/clicker.php
@@ -1,44 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="public/css/acid.css">
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+<script src="public/js/acid.js"></script>
 	<title>CLicker</title>
-	<style type="text/css">
-		#clicker{
-			background-color: red;
-			border-radius: 100%;
-			width: 200px;
-			height: 200px;
-			font-size: 20px;
-
-
-		}
-	</style>
-
+	
 </head>
 <body>
 	<button id="clicker" value="Click me"></button>
 	<div>
 		<input type="text" name="t1" id="t1" size="25">
 	</div>
-<script type="text/javascript">
-		score=-1;
-		var clicker = document.getElementById('clicker');
-		clicker.addEventListener('click', function(){
-			var msg = {
-				0: "stop",
-				1: "clicking",
-				2: "on",
-				3: "me",
-				4: "you",
-				5: "motherfucker",
-			};	
-			score++;
-			var i = score % 6;
-
-			document.getElementById('clicker').innerHTML = msg[i];
-			document.getElementById('t1').value = "score: " + score;
-		});
-		
-	</script>
 </body>
 </html>
